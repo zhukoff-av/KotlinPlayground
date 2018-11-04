ae6deaefc15b3ffdc0942f9145f5b0e7269a65af
zhukoff-av <mr.zhukoff@gmail.com>
2018-11-04 19:06:23 +0100

    add libraryFun

---
+++ Workshops/Task/src/otherFun/libraryFun.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    val s = "babc"
+
+    println(s.filter { it in 'a'..'b' }) // aba
+//    println(s.count { it in 'a'..'z' }) //3
+
+    "abCDa".any { it in '1'..'9' }  // false
+    "abCDa1".any { it in '1'..'9' } // true
+    // true if there at least one character satisfying the predicate
+
+    "abCDa".all { it.toInt() < 200 }
+    "abd33".find { it > 'c' }  // d
+
+    for ((index, ch) in "abc".withIndex()) {
+
+    }
+
+//    println("abcd".zip("1234"))
+
+    "abCDa".any { it in 'a'..'z' }
+    "abCDa".count { it in 'a'..'z' }
+    "abCDa".find { it in 'a'..'z' }
+    "abCDa".zip("79371").joinToString()
+}
